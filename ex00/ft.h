6bfde36d0b2d61a22884b16b4ae153a28749db06
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-31 11:21:01 +0200

    ex00

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,12 +6,17 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 11:15:43 by yrabby            #+#    #+#             */
-/*   Updated: 2022/05/31 11:15:50 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/05/31 11:20:54 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putchar(char c);
-void ft_swap(int *a, int *b);
-void ft_putstr(char *str);
-int ft_strlen(char *str);
-int ft_strcmp(char *s1, char *s2);
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
